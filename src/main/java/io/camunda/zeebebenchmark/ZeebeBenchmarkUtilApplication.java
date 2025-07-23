becdac046e2e5396585b2fda5f3b1a7c896cb7de
Adam Pounder <adam.pounder@camunda.com>
2025-07-23 16:09:40 +0100

    Fix missing metrics

--- src/main/java/io/camunda/zeebebenchmark/ZeebeBenchmarkUtilApplication.java
+++ src/main/java/io/camunda/zeebebenchmark/ZeebeBenchmarkUtilApplication.java
@@ -1,27 +1,14 @@
 package io.camunda.zeebebenchmark;
 
-import io.vavr.control.Try;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-
-import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ZeebeBenchmarkUtilApplication {
 
-	private static final CountDownLatch shutdownLatch = new CountDownLatch(1);
-
 	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(ZeebeBenchmarkUtilApplication.class);
-		springApplication.setWebApplicationType(WebApplicationType.NONE);
-		springApplication.run(args);
-		Try.run(shutdownLatch::await).get();
-	}
-	
-	public static void allowShutdown() {
-		shutdownLatch.countDown();
+		SpringApplication.run(ZeebeBenchmarkUtilApplication.class, args);;
 	}
 }
